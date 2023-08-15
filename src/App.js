9db38cbac8d919d1ad4a0670f5dd2f1222dd6a85
Maida Saniyah Lutfi <maidaasl@ip-192-168-1-9.ap-southeast-1.compute.internal>
2023-08-15 19:36:41 +0700

    feat: Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import {
+Card,
+} from "@material-tailwind/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+    <div className="mx-auto max-w-screen-md py-12">
+        <Card className="mb-12 overflow-hidden">
+
+        </Card>
+
+      </div>
+    </>
   );
 }
 
